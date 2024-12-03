b04958f07de831fb010d60c35c000f25546e3d08
Efi Panagopoulou <efipanagopoulou@Efis-MacBook-Air.local>
2024-12-03 10:54:43 +0100

    fix: Update the structure to backend and frontend folders

--- database/queries/sensors.js
+++
@@ -1,8 +0,0 @@
-const CREATE_SENSOR_DATA = `
-  INSERT INTO sensors_data (sensor_id, station_code, date, type, measurement, unit)
-  VALUES (?, ?, ?, ?, ?, ?)
-`;
-
-module.exports = {
-  CREATE_SENSOR_DATA
-};
--- database/queries/users.js
+++
@@ -1,7 +0,0 @@
-GET_USER = 'SELECT * FROM users WHERE email = ?';
-CREATE_USER = 'INSERT INTO users (email, password_hash, name) VALUES (?, ?, ?)';
-
-module.exports = {
-    GET_USER,
-    CREATE_USER
-}
